467585020995a857c77c972d3469e7c68245d8a2
saravanan574 <arulsaravanan461@gmail.com>
2025-12-17 02:23:46 +0530

    updated

---
+++ frontend/src/pages/Loader.jsx
@@ -0,0 +1,14 @@
+const Loader = () => {
+    return (
+      <div className="loader-overlay">
+        <div className="loader-card">
+          <div className="spinner"></div>
+          <h3>Loading Attendance System</h3>
+          <p>Please wait while we prepare your data</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
